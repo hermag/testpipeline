dc921666d273ac4c81c154dc3909530de24ffdcf
Erekle Magradze <erekle@magradze.de>
2023-03-07 21:48:07 +0400

    fix

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='EreklesPackage',
     version='1.0',
-    packages=['.EreklesPackage'],
+    packages=['EreklesPackage'],
     options = {"bdist_wheel": {"universal":True}}
 )
